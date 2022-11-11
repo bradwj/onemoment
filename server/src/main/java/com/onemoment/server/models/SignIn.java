a393a85f0f5bd936bbe4cb82a674f7c7a2c046fa
Elliana <ellianabrown18@gmail.com>
2022-11-11 09:14:36 -0600

    Sign In Use Case

---
+++ server/src/main/java/com/onemoment/server/models/SignIn.java
@@ -0,0 +1,64 @@
+package com.onemoment.server.models;
+
+import java.util.List;
+
+import com.onemoment.server.repositories.UserRepository;
+
+public class SignIn {
+    private String usernameAttempt;
+    private String passwordAttempt;
+    private UserRepository uR;
+    private User user;
+    private List<User> userList = uR.getAllUsers().getUserList();
+
+    
+    public SignIn(String uAttempt, String pAttempt)
+    {
+        user = null;
+        usernameAttempt = uAttempt;
+        passwordAttempt = pAttempt;
+    }
+
+    public void setUsername(String uAttempt)
+    {
+        usernameAttempt = uAttempt;
+    }
+
+    public void setPassword(String pAttempt)
+    {
+        usernameAttempt = pAttempt;
+    }
+
+    private boolean validUsername()
+    {
+        boolean vUsername = false;
+        for(User u : userList)
+        {
+            if(u.getUsername().equals(usernameAttempt))
+            {
+                vUsername = true;
+                user = u;
+            }
+        }
+        return vUsername;
+    }
+
+    private boolean correctPassword() 
+    {
+        boolean cPassword = false;
+        if(user != null)
+        {
+            if(user.getPassword().equals(passwordAttempt))
+            {
+                cPassword = true;
+            }
+        }
+        return cPassword;
+    }
+
+    public boolean isSuccessful()
+    {
+        return (validUsername() && correctPassword());
+    }
+}
+
