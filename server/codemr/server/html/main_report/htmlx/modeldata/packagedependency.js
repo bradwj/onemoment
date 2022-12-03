fb4cb4e64deb6a8d7ee3014240ac8f96afb3abd1
Brad Johnson <2021bradley@gmail.com>
2022-12-02 23:57:51 -0600

    Added CodeMR analysis output

---
+++ server/codemr/server/html/main_report/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,17 @@
+var matrix = [[0,0,0,0,0],[0,0,0,0,2],[0,1,0,1,2],[0,1,0,0,1],[0,0,0,0,0]]
+var packages = [{
+"name": " com.onemoment.server", "color": " #3182bd"
+}
+,{
+"name": " com.onemoment.server.repositories", "color": " #6baed6"
+}
+,{
+"name": " com.onemoment.server.controllers", "color": " #9ecae1"
+}
+,{
+"name": " com.onemoment.server.service", "color": " #c6dbef"
+}
+,{
+"name": " com.onemoment.server.models", "color": " #e6550d"
+}
+];
---
+++ server/codemr/server/html/workingsets.js
@@ -0,0 +1,3 @@
+var EQ_workingSetList = [
+{name: 'server.main', path:'servermain'}
+];
